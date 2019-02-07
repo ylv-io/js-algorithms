a26e4de8dccca6d1160800f48505b66b706a947c
Igor Yalovoy <igor@yalovoi.net>
2019-02-07 21:47:38 +0300

    leader

---
+++ src/challenges/dominator.js
@@ -0,0 +1,45 @@
+// An array A consisting of N integers is given. The dominator of array A is the value that occurs in more than half of the elements of A.
+
+// For example, consider array A such that
+
+//  A[0] = 3    A[1] = 4    A[2] =  3
+//  A[3] = 2    A[4] = 3    A[5] = -1
+//  A[6] = 3    A[7] = 3
+// The dominator of A is 3 because it occurs in 5 out of 8 elements of A (namely in those with indices 0, 2, 4, 6 and 7) and 5 is more than a half of 8.
+
+// Write a function
+
+// function solution(A);
+
+// that, given an array A consisting of N integers, returns index of any element of array A in which the dominator of A occurs. The function should return −1 if array A does not have a dominator.
+
+// For example, given array A such that
+
+//  A[0] = 3    A[1] = 4    A[2] =  3
+//  A[3] = 2    A[4] = 3    A[5] = -1
+//  A[6] = 3    A[7] = 3
+// the function may return 0, 2, 4, 6 or 7, as explained above.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [0..100,000];
+// each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+
+function solution(arr) {
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = ++obj[arr[i]] || 1;
+  }
+  let leader;
+  for (const key of Object.keys(obj)) {
+    if (obj[key] > obj[leader] || !obj[leader]) {
+      leader = Number.parseInt(key, 10);
+    }
+  }
+  return obj[leader] / arr.length > 0.5 ? arr.indexOf(leader) : -1;
+}
+
+console.log(solution([]));
+console.log(solution([1]));
+console.log(solution([1, 2, 3]));
+console.log(solution([3, 4, 3, 2, 3, -1, 3, 3]));
--- src/structures/graph.js
+++ src/structures/graph.js
@@ -23,13 +23,63 @@ class Graph {
     this.adjacencyList[v].forEach(o => this.removeEdge(v, o));
     delete this.adjacencyList[v];
   }
+
+  dfs(v) {
+    const visited = new Map();
+    const helper = (o) => {
+      visited.set(o, true);
+      this.adjacencyList[o].filter(i => !visited.get(i))
+        .forEach(i => helper(i));
+    };
+    helper(v);
+    return [...visited.keys()];
+  }
+
+  bfs(v) {
+    const visited = new Map();
+    const result = [];
+    const queue = [v];
+    let current;
+    visited.set(v, true);
+    while (queue.length) {
+      current = queue.shift();
+      result.push(current);
+
+      this.adjacencyList[current].forEach((o) => {
+        if (!visited.get(o)) {
+          visited.set(o, true);
+          queue.push(o);
+        }
+      });
+    }
+    return result;
+  }
 }
+
+//               Moscow
+//            /          \
+//         New York        Peterbug
+//          |               |
+//        Madrid  ------  London
+//           \             /
+//                Berlin
 
 const graph = new Graph();
 graph.addVertex('Moscow');
 graph.addVertex('New York');
 graph.addVertex('Peterburg');
+graph.addVertex('Madrid');
+graph.addVertex('London');
+graph.addVertex('Berlin');
 graph.addEdge('Moscow', 'New York');
 graph.addEdge('Moscow', 'Peterburg');
+graph.addEdge('New York', 'Madrid');
+graph.addEdge('Peterburg', 'London');
+graph.addEdge('Madrid', 'Berlin');
+graph.addEdge('Madrid', 'London');
+graph.addEdge('London', 'Berlin');
+
+console.log(graph.dfs('Moscow'));
+console.log(graph.bfs('Moscow'));
 // graph.removeEdge('Moscow', 'Peterburg');
 graph.removeVertex('Moscow');
